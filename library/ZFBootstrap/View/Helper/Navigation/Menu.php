b991befedd7649656f16ae64cf291be755e67191
Michael Moussa <michael.moussa@gmail.com>
2012-04-07 11:32:37 -0400

    Add "dropdown" attribute to dropdown trigger parent <li> element

--- library/ZFBootstrap/View/Helper/Navigation/Menu.php
+++ library/ZFBootstrap/View/Helper/Navigation/Menu.php
@@ -2,7 +2,10 @@
 
 namespace ZFBootstrap\View\Helper\Navigation;
 
-use Zend_View_Helper_Navigation_Menu;
+use DOMDocument,
+    DOMXPath,
+    Zend_Navigation_Container,
+    Zend_View_Helper_Navigation_Menu;
 
 /**
  * Adds support for the Twitter Bootstrap dropdown menus Javascript plugin
@@ -12,5 +15,41 @@ use Zend_View_Helper_Navigation_Menu;
  */
 class Menu extends Zend_View_Helper_Navigation_Menu
 {
+    /**
+     * Intercept render() call and apply custom Twitter Bootstrap class/id
+     * attributes.
+     *
+     * @see   Zend_View_Helper_Navigation_Menu::render()
+     * @param Zend_Navigation_Container $container (Optional) The navigation container.
+     *
+     * @return string
+     */
+    public function render(Zend_Navigation_Container $container = null)
+    {
+        return $this->applyBootstrapClassesAndIds(parent::render($container));
+    }
 
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Applies the custom Twitter Bootstrap dropdown class/id attributes where
+     * necessary.
+     *
+     * @param  string $html The HTML
+     * @return string
+     */
+    protected function applyBootstrapClassesAndIds($html)
+    {
+        $domDoc = new DOMDocument();
+        $domDoc->loadHTML($html);
+
+        $xpath = new DOMXPath($domDoc);
+
+        foreach ($xpath->query('//a[starts-with(@href, "#")]/..') as $item)
+        {
+            $item->setAttribute('class', 'dropdown');
+        }
+
+        return $domDoc->saveHTML();
+    }
 }
--- tests/library/ZFBootstrap/View/Helper/Navigation/MenuTest.php
+++ tests/library/ZFBootstrap/View/Helper/Navigation/MenuTest.php
@@ -2,7 +2,12 @@
 
 namespace ZFBootstrap\View\Helper\Navigation;
 
-use PHPUnit_Framework_TestCase,
+use DOMDocument,
+    DOMXPath,
+    PHPUnit_Framework_TestCase,
+    Zend_Navigation,
+    Zend_Navigation_Page_Uri,
+    Zend_View,
     ZFBootstrap\View\Helper\Navigation\Menu;
 
 /**
@@ -14,7 +19,80 @@ class MenuTest extends PHPUnit_Framework_TestCase
 {
     public function testMenuHelperExtendsZendMenuViewHelperClass()
     {
-        $this->assertInstanceOf('Zend_View_Helper_Navigation_Menu', new Menu(),
+        $this->assertInstanceOf('Zend_View_Helper_Navigation_Menu', $this->helper,
                                 'Menu helper should extend Zend_View_Helper_Navigation_Menu!');
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    public function testDropdownClassIsAppliedToFirstDropdownTriggerParentLiElement()
+    {
+        $domDoc = new DOMDocument();
+        $domDoc->loadHTML($this->helper->render($this->getTestMenu()));
+
+        $xpath = new DOMXPath($domDoc);
+
+        $result = $xpath->query('//a[@href="#dropdown1"]/..');
+
+        $this->assertSame('dropdown', $result->item(0)->getAttribute('class'),
+                          '#dropdown1 trigger parent <LI> element is missing the "dropdown" class!');
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // protected:
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Generates and returns a test menu.
+     *
+     * @return array
+     */
+    protected function getTestMenu()
+    {
+        $dropdownPages = array(array(new Zend_Navigation_Page_Uri(array('label' => 'Subpage 1-1',
+                                                                        'uri'   => '/subpage1-1')),
+                                     new Zend_Navigation_Page_Uri(array('label' => 'Subpage 1-2',
+                                                                        'uri'   => '/subpage1-2'))),
+                               array(new Zend_Navigation_Page_Uri(array('label' => 'Subpage 2-1',
+                                                                        'uri'   => '/subpage2-1'))));
+
+        $pages = array(new Zend_Navigation_Page_Uri(array('label' => 'Page 1',
+                                                          'uri'   => '/page1')),
+                       new Zend_Navigation_Page_Uri(array('label' => 'Page 2',
+                                                          'uri'   => '/page2')),
+                       new Zend_Navigation_Page_Uri(array('label' => 'Dropdown Trigger 1',
+                                                          'pages' => $dropdownPages[0],
+                                                          'uri'   => '#dropdown1')),
+                       new Zend_Navigation_Page_Uri(array('label' => 'Dropdown Trigger 2',
+                                                          'pages' => $dropdownPages[1],
+                                                          'uri'   => '#dropdown2')));
+        return new Zend_Navigation(array(new Zend_Navigation_Page_Uri(array('label' => 'Root',
+                                                                            'pages' => $pages,
+                                                                            'uri'   => '/'))));
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    protected function setUp()
+    {
+        $this->helper = new Menu();
+        $this->helper->setView(new Zend_View());
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    protected function tearDown()
+    {
+        $this->helper = null;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * The helper being tested.
+     *
+     * @var ZFBootstrap\View\Helper\Navigation\Menu
+     */
+    protected $helper = null;
 }
